61fcc3da9d8b699e2cee6619567e0cfc2c454406
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-29 00:26:27 +0530

    Generate static pages controller; configure routes for root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/results'
+
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
