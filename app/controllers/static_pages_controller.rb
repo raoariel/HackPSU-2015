6f268c1b02bb2fa12ff267916f2f7f8e26845573
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-29 01:28:11 +0530

    Add translation code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
+include StaticPagesHelper
 class StaticPagesController < ApplicationController
   def home
   end
 
   def results
   	uri = URI(params[:url])
-		@string = Net::HTTP.get(uri)
+		@code_original = Net::HTTP.get(uri)
+		@code_translated = run_translation(@code_original)
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,3 +1,20 @@
 module StaticPagesHelper
 	
+	def run_translation(code_original)
+		translator = MicrosoftTranslator::Client.new('GitATranslation', 'cwzLCkrF5eAIejqfZn8zx/QkDBESQMzo/OO/gzBRGtU=')
+		code_translated = code_original
+		code_original.split(/(#(.*)\n)|("""(.*)""")/).each do |x|
+			if x[0] == "#"
+				language = translator.detect(x)
+				translated = "# " + translator.translate(x,language,"es","text/html")
+				code_translated.gsub!(x, translated)
+			elsif x.include? "\"\"\""
+				x.gsub!("\"\"\"", "")
+				language = translator.detect(x)
+				translated = translator.translate(x,language,"es","text/html")
+				code_translated.gsub!(x, translated)
+			end
+		end
+		code_translated
+	end
 end
