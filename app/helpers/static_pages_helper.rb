c716c2ab37a38acd44fa2c2bad56da06ec0804c4
raoariel <arielrao@andrew.cmu.edu>
2015-05-20 18:41:47 -0700

    add regex for more languages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -5,6 +5,8 @@ module StaticPagesHelper
 		File.extname(uri.path)
 	end
 
+
+	# Still need multiline 
 	def run_translation(code_original, extension, language_code)
 		code_translated = code_original
 		translator = MicrosoftTranslator::Client.new('GitATranslation', 'cwzLCkrF5eAIejqfZn8zx/QkDBESQMzo/OO/gzBRGtU=')
@@ -14,6 +16,7 @@ module StaticPagesHelper
 					language = translator.detect(x)
 					translated = "#" + translator.translate(x,language, language_code,"text/html") + "\n"
 					code_translated.gsub!(x, translated)
+				# @TODO does not handle multiline comment
 				elsif x.include? "\"\"\""
 					x.gsub!("\"\"\"", "")
 					language = translator.detect(x)
@@ -26,6 +29,60 @@ module StaticPagesHelper
 				if x[0] == "#"
 					language = translator.detect(x)
 					translated = "#" + translator.translate(x,language, language_code,"text/html") + "\n"
+					code_translated.gsub!(x, translated)
+				end
+			end
+		elsif ((extension == '.r') || 
+			   (extension == '.coffee') || 
+			   (extension == '.pl'))
+			code_original.split(/(#.*\n)/).each do |x|
+				if x[0] == "#"
+					language = translator.detect(x)
+					translated = "#" + translator.translate(x,language, language_code,"text/html") + "\n"
+					code_translated.gsub!(x, translated)
+				end
+			end
+		elsif extension == '.hs'
+			code_original.split(/(\-\-.*\n)/).each do |x|
+				if x[0] == "\-\-"
+					language = translator.detect(x)
+					translated = "\-\-" + translator.translate(x,language, language_code,"text/html") + "\n"
+					code_translated.gsub!(x, translated)
+				end
+			end
+		elsif extension == '.m'
+			code_original.split(/(\%.*\n)/).each do |x|
+				if x[0] == "\%"
+					language = translator.detect(x)
+					translated = "\%" + translator.translate(x,language, language_code,"text/html") + "\n"
+					code_translated.gsub!(x, translated)
+				elsif x.include? "\.\.\."
+					x.gsub!("\.\.\.", "")
+					language = translator.detect(x)
+					translated = translator.translate(x,language, language_code,"text/html")
+					code_translated.gsub!(x, translated)
+				end
+			end
+		elsif extension == '.php'
+			code_original.split(/(#.*\n)/).each do |x|
+				if x[0] == "#"
+					language = translator.detect(x)
+					translated = "#" + translator.translate(x,language, language_code,"text/html") + "\n"
+					code_translated.gsub!(x, translated)
+				elsif x.include? "\/\/"
+					x.gsub!("\n", "")
+					language = translator.detect(x)
+					translated = translator.translate(x,language, language_code,"text/html")
+					code_translated.gsub!(x, translated)
+				end
+			end
+		elsif ((extension == '.c') || 
+			   (extension == '.java') || 
+			   (extension == '.js'))
+			code_original.split(/(\/\/.*\n)/).each do |x|
+				if x[0] == "\/\/"
+					language = translator.detect(x)
+					translated = "\/\/" + translator.translate(x,language, language_code,"text/html") + "\n"
 					code_translated.gsub!(x, translated)
 				end
 			end
